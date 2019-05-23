f4b5676f4b5362abeaddebc42051b388158eb1a6
mucheng <mucheng@muchengdeiMac.local>
2019-05-23 10:30:37 +0800

    [5.23]:finish tdd taxi

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var price = 0
+    if (inputs.distance < 2) {
+        price = 6 + inputs.parkTime * 0.25
+    } else if (inputs.distance > 2 && inputs.distance < 8) {
+        price = 6 + (inputs.distance - 2) * 0.8 + inputs.parkTime * 0.25
+    } else if (inputs.distance > 8) {
+        price = 6 + 6 * 0.8 + (inputs.distance - 8) * (0.8 + 0.8 * 0.5) + inputs.parkTime * 0.25
+    }
+    return Math.round(price);
 };
